33f54d24660920bf87367ae76eec806f7a4a8d0a
gaotianminzu <71334065+gaotianminzu@users.noreply.github.com>
2020-11-16 20:57:59 +0900

    Rubokop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Gimei.last.hiragana}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name  {Gimei.first.kanji}
-    last_name   {Gimei.last.kanji}
-    first_name_kana  {Gimei.first.katakana}
-    last_name_kana   {Gimei.last.katakana}
-    birth_date  {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    nickname              { Gimei.last.hiragana }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name  { Gimei.first.kanji }
+    last_name   { Gimei.last.kanji }
+    first_name_kana  { Gimei.first.katakana }
+    last_name_kana   { Gimei.last.katakana }
+    birth_date { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
-
 
 # FactoryBot.define do
 #   factory :user do
